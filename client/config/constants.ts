41e961846e0a61ca45fb0c3dd02494b0e7a5e1e5
Ofir Oss <ofiross@gmail.com>
2015-09-09 05:52:51 +0300

    fixed ts constants to match css

--- client/config/constants.ts
+++ client/config/constants.ts
@@ -4,10 +4,10 @@ export const TITLE_HEIGHT = 32;
 
 // Core = filter + split
 export const DIMENSION_HEIGHT = 27;
-export const CORE_ITEM_WIDTH = 200;
+export const CORE_ITEM_WIDTH = 192;
 export const CORE_ITEM_GAP = 8;
 
 export const PIN_TITLE_HEIGHT = 34;
 export const SEARCH_BOX_HEIGHT = 30;
 export const PIN_ITEM_HEIGHT = 24;
-export const PIN_PADDING_BOTTOM = 12;
+export const PIN_PADDING_BOTTOM = 10;
