67a383bd892e08fb4b631d4eb1a51f525f220cd0
Ofir Oss <ofiross@gmail.com>
2015-09-10 07:55:24 +0300

    module titles adjustments

--- client/config/constants.ts
+++ client/config/constants.ts
@@ -1,13 +1,13 @@
 'use strict';
 
-export const TITLE_HEIGHT = 32;
+export const TITLE_HEIGHT = 36;
 
 // Core = filter + split
 export const DIMENSION_HEIGHT = 27;
 export const CORE_ITEM_WIDTH = 192;
 export const CORE_ITEM_GAP = 8;
 
-export const PIN_TITLE_HEIGHT = 34;
+export const PIN_TITLE_HEIGHT = 36;
 export const SEARCH_BOX_HEIGHT = 30;
 export const PIN_ITEM_HEIGHT = 24;
 export const PIN_PADDING_BOTTOM = 10;
