a955888f5de2ef7b60d61d60edad409dca6fa808
Vadim Ogievetsky <vadimon@gmail.com>
2015-10-07 22:56:24 -0700

    polyfill in webpack

--- src/client/pivot.ts
+++ src/client/pivot.ts
@@ -11,6 +11,14 @@ import { queryUrlExecutorFactory } from './utils/ajax/ajax';
 import { Filter, Dimension, Measure, SplitCombine, Clicker, DataSource, DataSourceJS } from '../common/models/index';
 import { PivotApplication, PivotApplicationProps } from "./components/index";
 
+// Polyfill
+// from https://github.com/reppners/ios-html5-drag-drop-shim/tree/effectAllowed_dropEffect
+// /polyfill/mobile-drag-and-drop-polyfill/mobile-drag-and-drop-polyfill.js
+
+require.ensure(['../../assets/polyfill/ios-drag-drop.js'], (require) => {
+  require('../../assets/polyfill/ios-drag-drop.js');
+}, 'polyfill');
+
 export function pivot(container: Element, options: PivotApplicationProps) {
   var cont = document.getElementsByClassName('pivot-container')[0];
   if (!cont) return;
