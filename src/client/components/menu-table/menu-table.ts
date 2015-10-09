2b84782d147e51d44abb4a27a7ccf70ce8ac9a0f
Vadim Ogievetsky <vadimon@gmail.com>
2015-10-08 18:11:20 -0700

    basic search added to filter menu

--- src/client/components/menu-table/menu-table.ts
+++ src/client/components/menu-table/menu-table.ts
@@ -4,51 +4,108 @@ require('./menu-table.css');
 import { List } from 'immutable';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { $, Expression, Executor, Dataset, SortAction } from 'plywood';
+import { $, r, Expression, Executor, Dataset, SortAction } from 'plywood';
+import { SEGMENT } from '../../config/constants';
 import { formatterFromData } from '../../../common/utils/formatter/formatter';
 import { Essence, DataSource, Filter, Dimension, Measure, Clicker } from "../../../common/models/index";
 import { Checkbox } from '../checkbox/checkbox';
+import { Loader } from '../loader/loader';
 
 const TOP_N = 100;
+const MAX_SEARCH_LENGTH = 300;
+const SEARCH_WAIT = 900;
+
+function focusOnInput(input: HTMLInputElement): void {
+  if (!input) return;
+  input.focus();
+}
+
+function collect(wait: number, func: Function): Function {
+  var timeout: any;
+  var later = function() {
+    timeout = null;
+    func();
+  };
+  return function() {
+    if (!timeout) {
+      timeout = setTimeout(later, wait);
+    }
+  };
+}
 
 export interface MenuTableProps {
   essence: Essence;
   dimension: Dimension;
-  showSearch: boolean;
   showCheckboxes: boolean;
   selectedValues: List<string>;
   onValueClick: Function;
 }
 
 export interface MenuTableState {
+  loading?: boolean;
   dataset?: Dataset;
+  error?: any;
+  searchText?: string;
 }
 
 export class MenuTable extends React.Component<MenuTableProps, MenuTableState> {
   public mounted: boolean;
+  public collectTriggerSearch: Function;
 
   constructor() {
     super();
     this.state = {
-      dataset: null
+      loading: false,
+      dataset: null,
+      error: null,
+      searchText: ''
     };
+
+    this.collectTriggerSearch = collect(SEARCH_WAIT, () => {
+      if (!this.mounted) return;
+      var { essence, dimension } = this.props;
+      this.fetchData(essence, dimension);
+    });
   }
 
   fetchData(essence: Essence, dimension: Dimension): void {
+    var { searchText } = this.state;
     var { dataSource } = essence;
     var measure = essence.getPreviewSortMeasure();
 
+    var filterExpression = essence.getEffectiveFilter(null, dimension).toExpression();
+
+    if (searchText) {
+      filterExpression = filterExpression.and(dimension.expression.contains(r(searchText)));
+    }
+
     var query = $('main')
-      .filter(essence.getEffectiveFilter(null, dimension).toExpression())
-      .split(dimension.expression, dimension.name)
+      .filter(filterExpression)
+      .split(dimension.expression, SEGMENT)
       .performAction(measure.toApplyAction())
       .sort($(measure.name), SortAction.DESCENDING)
       .limit(TOP_N + 1);
 
-    dataSource.executor(query).then((dataset) => {
-      if (!this.mounted) return;
-      this.setState({ dataset });
-    });
+    this.setState({ loading: true });
+    dataSource.executor(query)
+      .then(
+        (dataset) => {
+          if (!this.mounted) return;
+          this.setState({
+            loading: false,
+            dataset,
+            error: null
+          });
+        },
+        (error) => {
+          if (!this.mounted) return;
+          this.setState({
+            loading: false,
+            dataset: null,
+            error
+          });
+        }
+      );
   }
 
   componentDidMount() {
@@ -75,12 +132,31 @@ export class MenuTable extends React.Component<MenuTableProps, MenuTableState> {
     this.mounted = false;
   }
 
+  onSearchChange(e: KeyboardEvent) {
+    this.setState({
+      searchText: (<HTMLInputElement>e.target).value.substr(0, MAX_SEARCH_LENGTH)
+    });
+    this.collectTriggerSearch();
+  }
+
+  highlightInString(str: string, searchText: string): any {
+    if (!searchText) return str;
+    var strLower = str.toLowerCase();
+    var startIndex = strLower.indexOf(searchText.toLowerCase());
+    if (startIndex === -1) return str;
+    var endIndex = startIndex + searchText.length;
+    return [
+      JSX(`<span className="pre"  key="pre" >{str.substring(0, startIndex)}</span>`),
+      JSX(`<span className="bold" key="bold">{str.substring(startIndex, endIndex)}</span>`),
+      JSX(`<span className="post" key="post">{str.substring(endIndex)}</span>`)
+    ];
+  }
+
   render() {
-    var { essence, dimension, showSearch, showCheckboxes, onValueClick, selectedValues } = this.props;
-    var { dataset } = this.state;
+    var { essence, showCheckboxes, onValueClick, selectedValues } = this.props;
+    var { loading, dataset, searchText } = this.state;
     var measure = essence.getPreviewSortMeasure();
 
-    var dimensionName = dimension.name;
     var measureName = measure.name;
 
     var rows: Array<React.DOMElement<any>> = [];
@@ -88,9 +164,17 @@ export class MenuTable extends React.Component<MenuTableProps, MenuTableState> {
     if (dataset) {
       hasMore = dataset.data.length > TOP_N;
       var rowData = dataset.data.slice(0, TOP_N);
+
+      if (searchText) {
+        var searchTextLower = searchText.toLowerCase();
+        rowData = rowData.filter((d) => {
+          return String(d[SEGMENT]).toLowerCase().indexOf(searchTextLower) !== -1;
+        });
+      }
+
       var formatter = formatterFromData(rowData.map(d => d[measureName]), measure.format);
       rows = rowData.map((d) => {
-        var segmentValue = d[dimensionName];
+        var segmentValue = d[SEGMENT];
         var segmentValueStr = String(segmentValue);
         var measureValue = d[measureName];
         var measureValueStr = formatter(measureValue);
@@ -107,7 +191,7 @@ export class MenuTable extends React.Component<MenuTableProps, MenuTableState> {
           <div className={'row' + (selected ? ' selected' : '')} key={segmentValueStr}>
             <div className="segment-value" onClick={onValueClick.bind(this, segmentValue)}>
               {checkbox}
-              <div className="label">{segmentValueStr}</div>
+              <div className="label">{this.highlightInString(segmentValueStr, searchText)}</div>
             </div>
             <div className="measure-value">{measureValueStr}</div>
           </div>
@@ -115,21 +199,31 @@ export class MenuTable extends React.Component<MenuTableProps, MenuTableState> {
       });
     }
 
+    var loader: React.ReactElement<any> = null;
+    if (loading) {
+      loader = React.createElement(Loader, null);
+    }
+
     var className = [
       'menu-table',
-      (hasMore ? 'has-more' : 'no-more'),
-      (showSearch ? 'with-search' : 'no-search')
+      (hasMore ? 'has-more' : 'no-more')
     ].join(' ');
-
-    var searchBar: React.DOMElement<any> = null;
-    if (showSearch) {
-      searchBar = JSX(`<div className="search"><input type="text" placeholder="Search"/></div>`);
-    }
 
     return JSX(`
       <div className={className}>
-        {searchBar}
-        <div className="rows">{rows}</div>
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchText}
+            onChange={this.onSearchChange.bind(this)}
+            ref={focusOnInput}
+          />
+        </div>
+        <div className="rows">
+          {rows}
+          {loader}
+        </div>
       </div>
     `);
   }
