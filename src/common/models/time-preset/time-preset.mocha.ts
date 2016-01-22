973c4acdd534c5eb4638d2ba61391f826d8f3976
evasomething <liou.eva@gmail.com>
2016-01-22 15:34:03 -0800

    fixed test

--- src/common/models/time-preset/time-preset.mocha.ts
+++ src/common/models/time-preset/time-preset.mocha.ts
@@ -10,22 +10,21 @@ describe('TimePreset', () => {
 
   it('is an immutable class', () => {
 
-    testImmutableClass(TimePreset,
-      [{
-        start: new Date('2013-02-26T19:00:00.000Z'),
+    testImmutableClass(TimePreset, [
+      {
         timeRange: {
           name: 'range1',
           start: new Date('2013-02-26T19:00:00.000Z'),
           end: new Date('2013-02-27T19:00:00.000Z')
         }
       },
-        {
+      {
+        timeRange: {
+          name: 'range2',
           start: new Date('2013-02-26T19:00:00.000Z'),
-          timeRange: {
-            name: 'range2',
-            start: new Date('2013-02-26T19:00:00.000Z'),
-            end: new Date('2013-02-27T19:00:00.000Z')
-          }
-        }]);
+          end: new Date('2013-02-27T19:00:00.000Z')
+        }
+      }
+    ]);
   });
 });
