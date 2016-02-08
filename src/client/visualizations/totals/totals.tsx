cf1a6d68ed2d170992ab864c3136da763d4199e2
Vadim Ogievetsky <vadimon@gmail.com>
2016-02-07 16:51:10 -0800

    bootstrap changes

--- src/client/visualizations/totals/totals.tsx
+++ src/client/visualizations/totals/totals.tsx
@@ -10,8 +10,8 @@ import { Stage, Essence, Splits, SplitCombine, Filter, Dimension, Measure, Color
 import { Loader } from '../../components/loader/loader';
 import { QueryError } from '../../components/query-error/query-error';
 
-const PADDING = 50;
-const TOTAL_WIDTH = 180;
+const PADDING_H = 100;
+const TOTAL_WIDTH = 176;
 
 export interface TotalsState {
   loading?: boolean;
@@ -101,8 +101,10 @@ export class Totals extends React.Component<VisualizationProps, TotalsState> {
     var { loading, dataset, error } = this.state;
 
     var myDatum = dataset ? dataset.data[0] : null;
+    var measures = essence.getMeasures();
+    var single = measures.size === 1;
 
-    var totals = essence.getMeasures().map(measure => {
+    var totals = measures.map(measure => {
       var measureName = measure.name;
 
       var measureValueStr = '-';
@@ -110,7 +112,10 @@ export class Totals extends React.Component<VisualizationProps, TotalsState> {
         measureValueStr = measure.formatFn(myDatum[measureName]);
       }
 
-      return <div className="total" key={measure.name}>
+      return <div
+        className={'total' + (single ? ' single' : '')}
+        key={measure.name}
+      >
         <div className="measure-name">{measure.title}</div>
         <div className="measure-value">{measureValueStr}</div>
       </div>;
@@ -126,12 +131,16 @@ export class Totals extends React.Component<VisualizationProps, TotalsState> {
       queryError = <QueryError error={error}/>;
     }
 
-    var numColumns = Math.min(totals.size, Math.max(1, Math.floor((stage.width - 2 * PADDING) / TOTAL_WIDTH)));
-    var containerWidth = numColumns * TOTAL_WIDTH;
-    var totalContainerStyle = {
-      width: containerWidth,
-      marginLeft: -containerWidth / 2
-    };
+    var totalContainerStyle: React.CSSProperties = null;
+    if (!single) {
+      var numColumns = Math.min(totals.size, Math.max(1, Math.floor((stage.width - 2 * PADDING_H) / TOTAL_WIDTH)));
+      var containerWidth = numColumns * TOTAL_WIDTH;
+      totalContainerStyle = {
+        left: '50%',
+        width: containerWidth,
+        marginLeft: -containerWidth / 2
+      };
+    }
 
     return <div className="totals">
       <div className="total-container" style={totalContainerStyle}>{totals}</div>
