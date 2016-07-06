432cf47b4ab49dd8b11a66d91046646518a1c948
eva <eva.liou@imply.io>
2016-07-06 13:24:23 -0700

    small change (#276)

--- src/common/utils/formatter/formatter.ts
+++ src/common/utils/formatter/formatter.ts
@@ -72,7 +72,7 @@ export function formatterFromData(values: number[], format: string): Formatter {
 }
 
 export function formatNumberRange(value: NumberRange) {
-  return `${formatValue(value.start)} to ${formatValue(value.end)}`;
+  return `${formatValue(value.start || `any`)} to ${formatValue(value.end  || `any`)}`;
 }
 
 export function formatValue(value: any, timezone?: Timezone, displayYear?: DisplayYear): string {
