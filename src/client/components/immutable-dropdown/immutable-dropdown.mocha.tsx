1eaad9feadedf82ffb23a2d111111e4be305d933
Sébastien <dolor_sit_amet@protonmail.ch>
2016-07-12 05:58:08 +0200

    Added tests for imutable inputs components (#284)

--- src/client/components/immutable-dropdown/immutable-dropdown.mocha.tsx
+++ src/client/components/immutable-dropdown/immutable-dropdown.mocha.tsx
@@ -4,22 +4,70 @@ import * as React from 'react';
 import * as TestUtils from 'react-addons-test-utils';
 import { $, Expression } from 'plywood';
 
-import { DataSourceMock, EssenceMock } from '../../../common/models/mocks';
+import { DataSourceMock } from '../../../common/models/mocks';
+import { DataSource, ListItem, Cluster } from '../../../common/models/index';
 
 import { findDOMNode } from '../../utils/test-utils/index';
 
 import { ImmutableDropdown } from './immutable-dropdown';
 
-describe.skip('ImmutableDropdown', () => {
+describe('ImmutableDropdown', () => {
+  var component: any;
+  var node: any;
+  var onChange: any;
+
+  beforeEach(() => {
+
+    onChange = sinon.spy();
+
+    var MyDropdown = ImmutableDropdown.specialize<ListItem>();
+
+    const engineTypes = Cluster.TYPE_VALUES.map(type => {return {value: type, label: type}; });
+
+    component = TestUtils.renderIntoDocument(
+      <MyDropdown
+        instance={DataSourceMock.twitter()}
+        path={'engine'}
+        label="Cluster"
+
+        onChange={onChange}
+
+        items={engineTypes}
+
+        equal={(a: ListItem, b: ListItem) => a.value === b.value}
+        renderItem={(a: ListItem) => a.label}
+        keyItem={(a: ListItem) => a.value}
+      />
+    );
+
+    node = findDOMNode(component) as any;
+  });
+
   it('adds the correct class', () => {
-    // var renderedComponent = TestUtils.renderIntoDocument(
-    //   <ImmutableDropdown
+    expect(TestUtils.isCompositeComponent(component), 'should be composite').to.equal(true);
+    expect(node.className, 'should contain class').to.contain('immutable-dropdown');
+  });
 
-    //   />
-    // );
 
-    // expect(TestUtils.isCompositeComponent(renderedComponent), 'should be composite').to.equal(true);
-    // expect((findDOMNode(renderedComponent) as any).className, 'should contain class').to.contain('immutable-dropdown');
+  it('selects an item and calls onChange', () => {
+    expect(onChange.callCount).to.equal(0);
+
+    TestUtils.Simulate.click(node);
+
+    var items = TestUtils.scryRenderedDOMComponentsWithClass(component, 'dropdown-item');
+
+    TestUtils.Simulate.click(items[1]);
+
+    expect(onChange.callCount).to.equal(1);
+
+    const args = onChange.args[0];
+
+    expect(args[0]).to.be.instanceOf(DataSource);
+    expect(args[0].engine).to.equal(Cluster.TYPE_VALUES[1]);
+
+    expect(args[1]).to.equal(true);
+
+    expect(args[2]).to.equal('engine');
   });
 
 });
--- src/client/components/immutable-input/immutable-input.mocha.tsx
+++ src/client/components/immutable-input/immutable-input.mocha.tsx
@@ -4,25 +4,75 @@ import * as React from 'react';
 import * as TestUtils from 'react-addons-test-utils';
 import { $, Expression } from 'plywood';
 
-import { DataSourceMock, EssenceMock } from '../../../common/models/mocks';
+import { DataSource } from '../../../common/models/index';
+import { DataSourceMock } from '../../../common/models/mocks';
 
 import { findDOMNode } from '../../utils/test-utils/index';
 
 import { ImmutableInput } from './immutable-input';
 
-describe.skip('ImmutableInput', () => {
-  it('adds the correct class', () => {
-    var myImmutableInstance: any;
+describe('ImmutableInput', () => {
+  var component: any;
+  var node: any;
+  var onChange: any;
+  var onInvalid: any;
 
-    var renderedComponent = TestUtils.renderIntoDocument(
+  beforeEach(() => {
+    onChange = sinon.spy();
+    onInvalid = sinon.spy();
+
+    component = TestUtils.renderIntoDocument(
       <ImmutableInput
-        instance={myImmutableInstance}
-        path={'pouet'}
+        instance={DataSourceMock.twitter()}
+        path={'engine'}
+        validator={/^[a-z]+$/}
+        onChange={onChange}
+        onInvalid={onInvalid}
       />
     );
 
-    expect(TestUtils.isCompositeComponent(renderedComponent), 'should be composite').to.equal(true);
-    expect((findDOMNode(renderedComponent) as any).className, 'should contain class').to.contain('immutable-input');
+    node = findDOMNode(component) as any;
   });
 
+  it('adds the correct class', () => {
+    expect(TestUtils.isCompositeComponent(component), 'should be composite').to.equal(true);
+    expect(node.className, 'should contain class').to.contain('immutable-input');
+  });
+
+  it('works for valid values', () => {
+    node.value = 'giraffe'; // Giraffes are the best engines
+    TestUtils.Simulate.change(node);
+
+    expect(onInvalid.callCount).to.equal(0);
+
+    expect(onChange.callCount).to.equal(1);
+    const args = onChange.args[0];
+
+    expect(args[0]).to.be.instanceOf(DataSource);
+    expect(args[0].engine).to.equal('giraffe');
+
+    expect(args[1]).to.equal(true);
+
+    expect(args[2]).to.equal('engine');
+  });
+
+  it('works for invalid values', () => {
+    node.value = '123';
+    TestUtils.Simulate.change(node);
+
+    expect(onInvalid.callCount).to.equal(1);
+    expect(onInvalid.args[0][0]).to.equal('123');
+
+    expect(onChange.callCount).to.equal(1);
+    const args = onChange.args[0];
+
+    expect(args[0]).to.be.instanceOf(DataSource);
+    expect(args[0].engine).to.equal(DataSourceMock.twitter().engine);
+
+    expect(args[1]).to.equal(false);
+
+    expect(args[2]).to.equal('engine');
+
+    expect(node.value).to.equal('123');
+  });
 });
