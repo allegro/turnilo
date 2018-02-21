ca2d112c9dcfeb11263b2de6692c6ac0d8367a5d
Piotr Szczepanik <piotr.szczepanik@allegro.pl>
2018-02-21 23:35:32 +0100

    Hot Module Reload working again

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -57,6 +57,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('dev-hrm')
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   ]
