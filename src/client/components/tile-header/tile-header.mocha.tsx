baf67164f701269527f856bf2dca0a0b79da5af1
Marcin Kuthan <marcin.kuthan@gmail.com>
2018-03-09 13:12:28 +0100

    Allegro copyright added to the source files. (#63)
    * Allegro copyright added to the source files.

--- src/client/components/tile-header/tile-header.mocha.tsx
+++ src/client/components/tile-header/tile-header.mocha.tsx
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015-2016 Imply Data, Inc.
+ * Copyright 2017-2018 Allegro.pl
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
