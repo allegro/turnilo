0378f473ad8cd8be50e572a765692b99676fe380
Adrian Mroz <adrian.mroz@allegro.pl>
2018-08-21 16:48:08 +0200

    simplify calculating limit dropdown inputs

--- src/client/components/split-menu/limit-dropdown.tsx
+++ src/client/components/split-menu/limit-dropdown.tsx
@@ -25,6 +25,22 @@ function formatLimit(limit: number | string): string {
   return limit === null ? "None" : String(limit);
 }
 
+const defaultLimits = [5, 10, 25, 50, 100];
+const limitsForColors = [3, 5, 7, 9, 10];
+
+function calculateSelectedLimit(limit: number, colors: Colors) {
+  if (!colors) return limit;
+  return colors.values ? "custom" : colors.limit;
+}
+
+function calculateLimits(colors: Colors, includeNone: boolean) {
+  const items = colors ? limitsForColors : defaultLimits;
+  if (includeNone) {
+    return items.concat([null]);
+  }
+  return items;
+}
+
 export interface LimitDropdownProps {
   limit: number;
   colors: Colors;
@@ -42,19 +58,10 @@ export const LimitDropdown: React.SFC<LimitDropdownProps> = ({ onLimitSelect, li
     onLimitSelect(limit, colors);
   }
 
-  let items: Array<number | string> = [5, 10, 25, 50, 100];
-  let selectedItem: number | string = limit;
-  if (colors) {
-    items = [3, 5, 7, 9, 10];
-    selectedItem = colors.values ? "custom" : colors.limit;
-  }
-
-  if (includeNone) items.unshift(null);
-
   return <Dropdown<number | string>
     label={STRINGS.limit}
-    items={items}
-    selectedItem={selectedItem}
+    items={calculateLimits(colors, includeNone)}
+    selectedItem={calculateSelectedLimit(limit, colors)}
     renderItem={formatLimit}
     onSelect={selectLimit}
   />;
