05741c59b78b655b37c4cd31314d71c622adb059
Adrian Mroz <adrian.mroz@allegro.pl>
2018-10-10 16:52:33 +0200

    Apply timeShift only for Splits on time Dimensions

--- src/common/models/split/split.ts
+++ src/common/models/split/split.ts
@@ -53,9 +53,9 @@ export function bucketToAction(bucket: Bucket): Expression {
     : new NumberBucketExpression({ size: bucket });
 }
 
-export function toExpression({ reference, bucket }: Split, filter?: Expression, shift?: Duration) {
+export function toExpression({ reference, bucket, type }: Split, filter?: Expression, shift?: Duration) {
   const ref = $(reference);
-  const withShift = shift && filter;
+  const withShift = shift && filter && type === SplitType.time;
   const expression = withShift ? filter.then(ref).fallback(ref.timeShift((shift))) : ref;
   if (!bucket) return expression;
   return expression.performAction(bucketToAction(bucket));
