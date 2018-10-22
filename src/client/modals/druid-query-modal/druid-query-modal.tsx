51baf1918ebda3d1705c06283800df790d5bc938
Adrian Mroz <adrian.mroz@allegro.pl>
2018-10-22 18:17:20 +0200

    Reformat common parts of druid query modal and view definition modal into source modal

--- src/client/modals/druid-query-modal/druid-query-modal.tsx
+++ src/client/modals/druid-query-modal/druid-query-modal.tsx
@@ -16,17 +16,11 @@
 
 import { External } from "plywood";
 import * as React from "react";
-import * as CopyToClipboard from "react-copy-to-clipboard";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { githubGist } from "react-syntax-highlighter/styles/hljs";
 import { Essence } from "../../../common/models/essence/essence";
 import { Timekeeper } from "../../../common/models/timekeeper/timekeeper";
 import { Fn } from "../../../common/utils/general/general";
 import makeQuery from "../../../common/utils/query/visualization-query";
-import { Button } from "../../components/button/button";
-import { Modal } from "../../components/modal/modal";
-import { STRINGS } from "../../config/constants";
-import "./druid-query-modal.scss";
+import { SourceModal } from "../source-modal/source-modal";
 
 interface DruidQueryModalProps {
   onClose: Fn;
@@ -34,45 +28,15 @@ interface DruidQueryModalProps {
   timekeeper: Timekeeper;
 }
 
-interface DruidQueryModalState {
-  copied: boolean;
-}
+export const DruidQueryModal: React.SFC<DruidQueryModalProps> = ({ onClose, timekeeper, essence }) => {
+  const { dataCube: { attributes, source } } = essence;
+  const query = makeQuery(essence, timekeeper);
+  const external = External.fromJS({ engine: "druid", attributes, source });
+  const plan = query.simulateQueryPlan({ main: external });
+  const planSource = JSON.stringify(plan, null, 2);
 
-export class DruidQueryModal extends React.Component<DruidQueryModalProps, DruidQueryModalState> {
-
-  state: DruidQueryModalState = { copied: false };
-
-  onCopy = () => this.setState({ copied: true });
-
-  render() {
-    const { onClose, timekeeper, essence } = this.props;
-    const { dataCube: { attributes, source } } = essence;
-    const query = makeQuery(essence, timekeeper);
-    const external = External.fromJS({ engine: "druid", attributes, source });
-    const plan = query.simulateQueryPlan({ main: external });
-    const planString = JSON.stringify(plan, null, 2);
-
-    return <Modal
-      onClose={onClose}
-      title="Druid query"
-      className="druid-query-modal"
-    >
-      <div className="content">
-        <SyntaxHighlighter
-          className="druid-query"
-          language="json"
-          style={githubGist}
-        >
-          {planString}
-        </SyntaxHighlighter>
-        <div className="button-bar">
-          <Button type="primary" className="close" onClick={onClose} title={STRINGS.close} />
-          <CopyToClipboard text={planString} onCopy={this.onCopy}>
-            <Button type="secondary" title={STRINGS.copyDefinition} />
-          </CopyToClipboard>
-          {this.state.copied ? <div className="copied-hint">Copied!</div> : null}
-        </div>
-      </div>
-    </Modal>;
-  }
-}
+  return <SourceModal
+    onClose={onClose}
+    title="Druid query"
+    source={planSource} />;
+};
--- src/client/modals/view-definition-modal/view-definition-modal.tsx
+++ src/client/modals/view-definition-modal/view-definition-modal.tsx
@@ -15,88 +15,30 @@
  */
 
 import * as React from "react";
-import * as CopyToClipboard from "react-copy-to-clipboard";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { githubGist } from "react-syntax-highlighter/styles/hljs";
 import { Essence } from "../../../common/models/essence/essence";
 import { Fn, makeTitle } from "../../../common/utils/general/general";
-
-import { DEFAULT_VIEW_DEFINITION_VERSION, defaultDefinitionConverter, ViewDefinition, ViewDefinitionVersion } from "../../../common/view-definitions";
-import { Button } from "../../components/button/button";
-import { Modal } from "../../components/modal/modal";
+import { DEFAULT_VIEW_DEFINITION_VERSION, defaultDefinitionConverter } from "../../../common/view-definitions";
 import { STRINGS } from "../../config/constants";
-
-import "./view-definition-modal.scss";
+import { SourceModal } from "../source-modal/source-modal";
 
 export interface ViewDefinitionModalProps {
   onClose: Fn;
   essence: Essence;
 }
 
-export interface ViewDefinitionModalState {
-  copied: boolean;
-}
+export const ViewDefinitionModal: React.SFC<ViewDefinitionModalProps> = () => {
 
-class MakeUrlData {
-  private dataCubeName: string;
-  private viewDefinitionVersion: ViewDefinitionVersion;
-  private viewDefinition: ViewDefinition;
+  const { essence, onClose } = this.props;
+  const viewDefinition = {
+    dataCubeName: essence.dataCube.name,
+    viewDefinitionVersion: DEFAULT_VIEW_DEFINITION_VERSION,
+    viewDefinition: defaultDefinitionConverter.toViewDefinition(essence)
+  };
+  const viewDefinitionAsJson = JSON.stringify(viewDefinition, null, 2);
 
-  constructor(dataCubeName: string, viewDefinitionVersion: ViewDefinitionVersion, viewDefinition: ViewDefinition) {
-    this.dataCubeName = dataCubeName;
-    this.viewDefinitionVersion = viewDefinitionVersion;
-    this.viewDefinition = viewDefinition;
-  }
-
-  public printAsJson(): string {
-    return JSON.stringify(this, null, 2);
-  }
-}
-
-export class ViewDefinitionModal extends React.Component<ViewDefinitionModalProps, ViewDefinitionModalState> {
-
-  constructor(props: ViewDefinitionModalProps) {
-    super(props);
-    this.state = {
-      copied: false
-    };
-  }
-
-  onCopyClick = () => {
-    this.setState({ copied: true });
-  }
-
-  render() {
-    const { essence, onClose } = this.props;
-    const { copied } = this.state;
-    const title = `${makeTitle(STRINGS.viewDefinition)}`;
-
-    const makeUrlData = new MakeUrlData(essence.dataCube.name, DEFAULT_VIEW_DEFINITION_VERSION, defaultDefinitionConverter.toViewDefinition(essence));
-    const viewDefinitionAsJson = makeUrlData.printAsJson();
-
-    return <Modal
-      className="view-definition-modal"
-      title={title}
-      onClose={onClose}
-    >
-      <div className="content">
-        {STRINGS.viewDefinitionSubtitle}
-        <SyntaxHighlighter
-          className="definition-view"
-          language="json"
-          style={githubGist}
-        >
-          {viewDefinitionAsJson}
-        </SyntaxHighlighter>
-        <div className="button-bar">
-          <Button type="primary" className="close" onClick={onClose} title={STRINGS.close} />
-
-          <CopyToClipboard text={viewDefinitionAsJson} onCopy={this.onCopyClick}>
-            <Button type="secondary" title={STRINGS.copyDefinition} />
-          </CopyToClipboard>
-          {copied ? <div className="copied-hint">Copied!</div> : null}
-        </div>
-      </div>
-    </Modal>;
-  }
-}
+  return <SourceModal
+    onClose={onClose}
+    header={STRINGS.viewDefinitionSubtitle}
+    title={`${makeTitle(STRINGS.viewDefinition)}`}
+    source={viewDefinitionAsJson} />;
+};
