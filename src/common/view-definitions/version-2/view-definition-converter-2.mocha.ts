6f14edd25be7aa3ddfc67efc777da2cd896cefb3
Richard Jones <itszootime@gmail.com>
2019-01-08 08:48:25 +0100

    fix v2 view conversion
    fixes for converting splits and filters which use time or dimensions with lookups,
    fallbacks, etc.

--- src/common/view-definitions/version-2/view-definition-converter-2.mocha.ts
+++ src/common/view-definitions/version-2/view-definition-converter-2.mocha.ts
@@ -15,6 +15,7 @@
  */
 
 import { expect } from "chai";
+import { Duration } from "chronoshift";
 import { MANIFESTS } from "../../manifests";
 import { DataCubeFixtures } from "../../models/data-cube/data-cube.fixtures";
 import { TimeFilterPeriod } from "../../models/filter-clause/filter-clause";
@@ -24,7 +25,7 @@ import { ViewDefinitionConverter2 } from "./view-definition-converter-2";
 
 describe("ViewDefinitionConverter2", () => {
 
-  const totalsWithTimeBucket: ViewDefinition2 = {
+  const totalsWithCurrentTimeBucket: ViewDefinition2 = {
     visualization: "totals",
     timezone: "Etc/UTC",
     filter: {
@@ -50,11 +51,227 @@ describe("ViewDefinitionConverter2", () => {
     pinnedSort: "delta"
   };
 
-  it("should convert time bucket expression to time range", () => {
-    const essence = new ViewDefinitionConverter2().fromViewDefinition(totalsWithTimeBucket, DataCubeFixtures.wiki(), MANIFESTS);
+  it("converts current time bucket expression to time period", () => {
+    const essence = new ViewDefinitionConverter2().fromViewDefinition(totalsWithCurrentTimeBucket, DataCubeFixtures.wiki(), MANIFESTS);
     const convertedClause = essence.filter.clauses.first();
 
-    const expectedClause = FilterClauseFixtures.timePeriod("time", "P1D", TimeFilterPeriod.LATEST);
+    const expectedClause = FilterClauseFixtures.timePeriod("time", "P1D", TimeFilterPeriod.CURRENT);
     expect(convertedClause).to.deep.equal(expectedClause);
   });
+
+  const totalsWithLatestTimeRange: ViewDefinition2 = {
+    visualization: "totals",
+    timezone: "Etc/UTC",
+    filter: {
+      op: "overlap",
+      operand: {
+        op: "ref",
+        name: "time"
+      },
+      expression: {
+        op: "timeRange",
+        operand: {
+          op: "ref",
+          name: "m"
+        },
+        duration: "P1D",
+        step: -3
+      }
+    },
+    splits: [],
+    singleMeasure: "delta",
+    multiMeasureMode: true,
+    selectedMeasures: ["count"],
+    pinnedDimensions: [],
+    pinnedSort: "delta"
+  };
+
+  it("converts latest time range expression to time period", () => {
+    const essence = new ViewDefinitionConverter2().fromViewDefinition(totalsWithLatestTimeRange, DataCubeFixtures.wiki(), MANIFESTS);
+    const convertedClause = essence.filter.clauses.first();
+
+    const expectedClause = FilterClauseFixtures.timePeriod("time", "P3D", TimeFilterPeriod.LATEST);
+    expect(convertedClause).to.deep.equal(expectedClause);
+  });
+
+  const totalsWithPreviousTimeRange: ViewDefinition2 = {
+    visualization: "totals",
+    timezone: "Etc/UTC",
+    filter: {
+      op: "overlap",
+      operand: {
+        op: "ref",
+        name: "time"
+      },
+      expression: {
+        op: "timeRange",
+        operand: {
+          op: "timeFloor",
+          operand: {
+            op: "ref",
+            name: "n"
+          },
+          duration: "P1W"
+        },
+        duration: "P1W",
+        step: -1
+      }
+    },
+    splits: [],
+    singleMeasure: "delta",
+    multiMeasureMode: true,
+    selectedMeasures: ["count"],
+    pinnedDimensions: [],
+    pinnedSort: "delta"
+  };
+
+  it("converts previous time bucket expression to time period", () => {
+    const essence = new ViewDefinitionConverter2().fromViewDefinition(totalsWithPreviousTimeRange, DataCubeFixtures.wiki(), MANIFESTS);
+    const convertedClause = essence.filter.clauses.first();
+
+    const expectedClause = FilterClauseFixtures.timePeriod("time", "P1W", TimeFilterPeriod.PREVIOUS);
+    expect(convertedClause).to.deep.equal(expectedClause);
+  });
+
+  const totalsWithSplits: ViewDefinition2 = {
+    visualization: "table",
+    timezone: "Etc/UTC",
+    filter: {
+      op: "chain",
+      expression: {
+        op: "ref",
+        name: "time"
+      },
+      actions: [
+        {
+          action: "in",
+          expression: {
+            op: "chain",
+            expression: {
+              op: "ref",
+              name: "n"
+            },
+            actions: [
+              {
+                action: "timeFloor",
+                duration: "P1W"
+              },
+              {
+                action: "timeRange",
+                duration: "P1W",
+                step: -1
+              }
+            ]
+          }
+        },
+        {
+          action: "and",
+          expression: {
+            op: "chain",
+            expression: {
+              op: "ref",
+              name: "page"
+            },
+            actions: [
+              {
+                action: "lookup",
+                lookup: "page_last_author"
+              },
+              {
+                action: "overlap",
+                expression: {
+                  op: "literal",
+                  value: {
+                    setType: "STRING",
+                    elements: [
+                      "TypeScript"
+                    ]
+                  },
+                  type: "SET"
+                }
+              }
+            ]
+          }
+        }
+      ]
+    },
+    splits: [
+      {
+        expression: {
+          op: "chain",
+          expression: {
+            op: "ref",
+            name: "page"
+          },
+          actions: [
+            {
+              action: "lookup",
+              lookup: "page_last_author"
+            }
+          ]
+        },
+        sortAction: {
+          action: "sort",
+          expression: {
+            op: "ref",
+            name: "count"
+          },
+          direction: "descending"
+        },
+        limitAction: {
+          action: "limit",
+          limit: 10
+        }
+      },
+      {
+        expression: {
+          op: "ref",
+          name: "time"
+        },
+        bucketAction: {
+          action: "timeBucket",
+          duration: "PT1H"
+        },
+        sortAction: {
+          action: "sort",
+          expression: {
+            op: "ref",
+            name: "time"
+          },
+          direction: "ascending"
+        }
+      }
+    ],
+    multiMeasureMode: true,
+    singleMeasure: "count",
+    selectedMeasures: [
+      "count"
+    ],
+    pinnedDimensions: [],
+    pinnedSort: "count",
+    colors: null,
+    compare: null,
+    highlight: null
+  };
+
+  it("converts filter with lookup expressions", () => {
+    const convertedFilter = new ViewDefinitionConverter2().fromViewDefinition(totalsWithSplits, DataCubeFixtures.wiki(), MANIFESTS).filter;
+    const convertedClause = convertedFilter.clauses.get(1);
+
+    const expectedClause = FilterClauseFixtures.stringIn("page_last_author", ["TypeScript"]);
+    expect(convertedClause).to.deep.equal(expectedClause);
+  });
+
+  it("converts splits with lookup expressions", () => {
+    const convertedSplits = new ViewDefinitionConverter2().fromViewDefinition(totalsWithSplits, DataCubeFixtures.wiki(), MANIFESTS).splits;
+    expect(convertedSplits.length()).to.equal(2);
+    expect(convertedSplits.getSplit(0).reference).to.equal("page_last_author");
+  });
+
+  it("converts time bucket splits", () => {
+    const convertedSplits = new ViewDefinitionConverter2().fromViewDefinition(totalsWithSplits, DataCubeFixtures.wiki(), MANIFESTS).splits;
+    expect(convertedSplits.length()).to.equal(2);
+    expect(convertedSplits.getSplit(1).bucket).to.deep.equal(Duration.fromJS("PT1H"));
+  });
+
 });
