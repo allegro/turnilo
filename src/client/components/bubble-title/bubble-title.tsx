eb383cb068c3dc0d31ca7ae5df84d75d1f2d713d
Adrian Mroz <adrian.mroz@allegro.pl>
2019-02-11 17:13:19 +0100

    HighlightModal and inner components

---
+++ src/client/components/bubble-title/bubble-title.tsx
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017-2018 Allegro.pl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from "react";
+import { clamp } from "../../utils/dom/dom";
+
+const PER_LETTER_PIXELS = 8;
+const MIN_TITLE_WIDTH = 80;
+const MAX_TITLE_WIDTH = 300;
+
+interface BubbleTitleProps {
+  title: string;
+}
+
+export const BubbleTitle: React.SFC<BubbleTitleProps> = ({ title }) => {
+  const minWidth = clamp(title.length * PER_LETTER_PIXELS, MIN_TITLE_WIDTH, MAX_TITLE_WIDTH);
+  return <div className="title" style={{ minWidth }}>{title}</div>;
+};
