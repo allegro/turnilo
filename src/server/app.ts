1fedc130323a75a0323ce3af83a770ceaeb7f165
Adrian Mroz <adrian.mroz@allegro.pl>
2019-02-19 12:15:31 +0100

    Remove route for changing settings and dependent code

--- src/server/app.ts
+++ src/server/app.ts
@@ -29,34 +29,10 @@ import * as healthRoutes from "./routes/health/health";
 import * as mkurlRoutes from "./routes/mkurl/mkurl";
 import * as plyqlRoutes from "./routes/plyql/plyql";
 import * as plywoodRoutes from "./routes/plywood/plywood";
-import * as settingsRoutes from "./routes/settings/settings";
 import * as shortenRoutes from "./routes/shorten/shorten";
 import * as swivRoutes from "./routes/swiv/swiv";
 import { SwivRequest } from "./utils/general/general";
 import { errorLayout } from "./views";
-
-function makeGuard(guard: string): Handler {
-  return (req: SwivRequest, res: Response, next: Function) => {
-    const user = req.user;
-    if (!user) {
-      next(new Error("no user"));
-      return;
-    }
-
-    const { allow } = user;
-    if (!allow) {
-      next(new Error("no user.allow"));
-      return;
-    }
-
-    if (!allow[guard]) {
-      next(new Error("not allowed"));
-      return;
-    }
-
-    next();
-  };
-}
 
 var app = express();
 app.disable("x-powered-by");
@@ -68,12 +44,6 @@ if (SERVER_SETTINGS.getTrustProxy() === "always") {
 function addRoutes(attach: string, router: Router | Handler): void {
   app.use(attach, router);
   app.use(SERVER_SETTINGS.getServerRoot() + attach, router);
-}
-
-function addGuardedRoutes(attach: string, guard: string, router: Router | Handler): void {
-  var guardHandler = makeGuard(guard);
-  app.use(attach, guardHandler, router);
-  app.use(SERVER_SETTINGS.getServerRoot() + attach, guardHandler, router);
 }
 
 // Add compression
@@ -137,11 +107,9 @@ app.use(bodyParser.urlencoded({
 }));
 
 // Assign basics
-var stateful = SETTINGS_MANAGER.isStateful();
 app.use((req: SwivRequest, res: Response, next: Function) => {
   req.user = null;
   req.version = VERSION;
-  req.stateful = stateful;
   req.getSettings = (opts: GetSettingsOptions = {}) => {
     return SETTINGS_MANAGER.getSettings(opts);
   };
@@ -188,9 +156,6 @@ addRoutes("/plyql", plyqlRoutes);
 addRoutes("/mkurl", mkurlRoutes);
 addRoutes("/shorten", shortenRoutes);
 addRoutes("/error", errorRoutes);
-if (stateful) {
-  addGuardedRoutes("/settings", "settings", settingsRoutes);
-}
 
 // View routes
 if (SERVER_SETTINGS.getIframe() === "deny") {
