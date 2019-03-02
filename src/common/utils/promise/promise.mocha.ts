30967389182e9c577d13c2e0250129520255d042
Adrian Mroz <adrian.mroz@allegro.pl>
2019-03-02 20:54:23 +0100

    deferred implementation on es6 promises

--- src/common/utils/promise/promise.mocha.ts
+++ src/common/utils/promise/promise.mocha.ts
@@ -17,17 +17,50 @@
 import { expect } from "chai";
 import { spy } from "sinon";
 import { sleep } from "../../../client/utils/test-utils";
-import { timeout } from "./promise";
+import { Deferred, timeout } from "./promise";
 
-describe("timeout", () => {
-  it("should reject after defined timeout in ms", async () => {
-    const successSpy = spy();
-    const failureSpy = spy();
-    timeout(10).then(successSpy).catch(failureSpy);
-    expect(successSpy.called).to.be.false;
-    expect(failureSpy.called).to.be.false;
-    await sleep(10);
-    expect(successSpy.called).to.be.false;
-    expect(failureSpy.called).to.be.true;
+describe("Promise utils", () => {
+  describe("timeout", () => {
+    it("should reject after defined timeout in ms", async () => {
+      const successSpy = spy();
+      const failureSpy = spy();
+      timeout(10).then(successSpy).catch(failureSpy);
+      expect(successSpy.called).to.be.false;
+      expect(failureSpy.called).to.be.false;
+      await sleep(10);
+      expect(successSpy.called).to.be.false;
+      expect(failureSpy.called).to.be.true;
+    });
+  });
+
+  describe("Deferred", () => {
+    it("should resolve with method", async () => {
+      const onResolve = spy();
+      const deferred = new Deferred<string>();
+      const promise = deferred.promise;
+      promise.then(onResolve);
+
+      deferred.resolve("resolve");
+      await promise;
+      expect(onResolve.calledOnce).to.be.true;
+      expect(onResolve.calledWith("resolve")).to.be.true;
+    });
+
+    it("should have a method to reject reject a promise", async () => {
+      const onReject = spy();
+      const deferred = new Deferred<string>();
+      const promise = deferred.promise;
+      promise.catch(onReject);
+
+      deferred.reject("reject");
+      try {
+        await promise;
+      } catch {
+        // empty catch for promise rejection
+      }
+      expect(onReject.calledOnce).to.be.true;
+      expect(onReject.calledWith("reject")).to.be.true;
+    });
+
   });
 });
