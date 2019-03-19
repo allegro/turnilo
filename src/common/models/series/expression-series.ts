b8207d6cbe85ebd5db6bcaadb3b3b708d6ffcc60
Adrian Mroz <adrian.mroz@allegro.pl>
2019-03-19 12:42:21 +0100

    fromMeasure for ExpressionSeries for creating placeholder series

--- src/common/models/series/expression-series.ts
+++ src/common/models/series/expression-series.ts
@@ -16,11 +16,14 @@
 
 import { Record } from "immutable";
 import { RequireOnly } from "../../utils/functional/functional";
+import { Measure } from "../measure/measure";
 import { BasicSeriesValue, SeriesBehaviours } from "./series";
 import { DEFAULT_FORMAT, SeriesFormat } from "./series-format";
 import { SeriesType } from "./series-type";
 
 export enum ExpressionSeriesOperation { PERCENT_OF_PARENT = "percent_of_parent", PERCENT_OF_TOTAL = "percent_of_total" }
+
+const DEFAULT_OPERATION: ExpressionSeriesOperation = ExpressionSeriesOperation.PERCENT_OF_PARENT;
 
 interface ExpressionSeriesValue extends BasicSeriesValue {
   type: SeriesType.EXPRESSION;
@@ -34,10 +37,14 @@ const defaultSeries: ExpressionSeriesValue = {
   reference: null,
   format: DEFAULT_FORMAT,
   type: SeriesType.EXPRESSION,
-  operation: null
+  operation: DEFAULT_OPERATION
 };
 
-export class ExpressionSeries extends Record<ExpressionSeriesValue>(defaultSeries)  implements SeriesBehaviours {
+export class ExpressionSeries extends Record<ExpressionSeriesValue>(defaultSeries) implements SeriesBehaviours {
+
+  static fromMeasure({ name }: Measure) {
+    return new ExpressionSeries({ reference: name, operation: null });
+  }
 
   constructor(params: RequireOnly<ExpressionSeriesValue, "reference" | "operation">) {
     super(params);
