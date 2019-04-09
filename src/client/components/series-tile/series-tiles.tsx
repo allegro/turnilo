7d97d1f41c795b533ab5d9e6a0ba71bf9ae4b2a2
Adrian Mroz <adrian.mroz@allegro.pl>
2019-04-09 13:32:04 +0200

    Fix typo

--- src/client/components/series-tile/series-tiles.tsx
+++ src/client/components/series-tile/series-tiles.tsx
@@ -102,7 +102,7 @@ export const SeriesTiles: React.SFC<SeriesTilesProps> = props => {
   const overflowItems = seriesElements.slice(maxItems);
   if (overflowItems.length <= 0) return <React.Fragment>{visibleItems}</React.Fragment>;
 
-  const anyOverflowItemOpen = series.slice(maxItems).some(({ series }) => series.equals(openSeriesMenu));
+  const anyOverflowItemOpen = series.slice(maxItems).some(({ series }) => series.equals(openedSeriesMenu));
   const isDummySeriesInOverflow = overflowItems.some(element => element.type === PlaceholderSeriesTile);
   const overflowOpened = overflowOpen || anyOverflowItemOpen || isDummySeriesInOverflow;
 
