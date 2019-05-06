cf27cd21a6473e98df7a2e9b292214db55a4aba9
Adrian Mroz <adrian.mroz@allegro.pl>
2019-05-06 16:10:01 +0200

    Simplify constraints on measure vs series

--- src/common/manifests/heat-map/heat-map.ts
+++ src/common/manifests/heat-map/heat-map.ts
@@ -109,7 +109,7 @@ const suggestAddingMeasure = ({ dataCube, series }: ActionVariables) => [{
 const suggestRemovingMeasures = ({ series }: ActionVariables) => [{
   description: series.count() === 2 ? "Remove last measure" : `Remove last ${series.count() - 1} measures`,
   adjustment: {
-    series: series.shift()
+    series: series.takeFirst()
   }
 }];
 
