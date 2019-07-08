63ddc2868a86a4f9a1f7f628f9c4737a070a19c4
Cedric Sohrauer <sohrauer@gmail.com>
2019-07-08 11:40:41 +0200

    restyle table to separate segment and content
    and make the resize handle full height

--- src/client/visualizations/table/table.tsx
+++ src/client/visualizations/table/table.tsx
@@ -472,13 +472,6 @@ export class Table extends BaseVisualization<TableState> {
 
     const corner = <div className="corner">
       <div className="corner-wrap">{segmentTitle}</div>
-      <ResizeHandle
-        direction={Direction.LEFT}
-        onResize={this.setSegmentWidth}
-        min={this.defaultSegmentWidth()}
-        max={MAX_SEGMENT_WIDTH}
-        value={segmentWidth}
-      />
     </div>;
 
     const measuresCount = essence.getConcreteSeries().size;
@@ -496,6 +489,13 @@ export class Table extends BaseVisualization<TableState> {
     };
 
     return <div className="internals table-inner">
+      <ResizeHandle
+        direction={Direction.LEFT}
+        onResize={this.setSegmentWidth}
+        min={this.defaultSegmentWidth()}
+        max={MAX_SEGMENT_WIDTH}
+        value={segmentWidth}
+      />
       <Scroller
         ref="scroller"
         layout={scrollerLayout}
