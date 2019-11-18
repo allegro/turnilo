f5532cc12d3d5ffa44698eaa8823ab2e66d918d1
Adrian Mroz <adrian.mroz@allegro.pl>
2019-11-18 18:41:04 +0100

    Home Page tests suite

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -5,10 +5,33 @@ context('Home Page', () => {
     cy.visit('http://localhost:9090');
   });
 
-  it('test', () => {
+  it('should load wikipedia cube', () => {
+    cy.get('.cube-header-bar .title')
+      .should('contain', 'Wikipedia Example');
+  });
 
-    cy.get('.total .measure-value')
-      .should("have.length", 1)
-      .should('contain', '9.4 m');
-  })
+  it('should load Totals visualisation', () => {
+    cy.get('.base-visualization')
+      .should('have.class', 'totals');
+  });
+
+  it('should set Latest day time filter', () => {
+    cy.get('.filter-tile .filter')
+      .should('have.length', 1)
+      .should('contain', 'Latest day');
+  });
+
+  it('should set default series "Added"', () => {
+    cy.get('.series-tile .series')
+      .should('have.length', 1)
+      .should('contain', 'Added');
+  });
+
+  it('should load data for defined filters and measures', async () => {
+    const total = await cy.get('.visualization .total');
+    const name = total.find('.measure-name');
+    const value = total.find('.measure-value');
+    expect(name).to.have.text('Added');
+    expect(value).to.have.text('9.4 m');
+  });
 });
