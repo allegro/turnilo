dd9a15847b0ff5512d94b7abf616c4792e54b772
Adrian Mroz <adrian.mroz@allegro.pl>
2019-11-29 11:51:24 +0100

    Fix linter rule for header and fix copyright dates in headers

--- src/client/utils/data-cubes-filter/data-cubes-filter.mocha.ts
+++ src/client/utils/data-cubes-filter/data-cubes-filter.mocha.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 Allegro.pl
+ * Copyright 2017-2019 Allegro.pl
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
