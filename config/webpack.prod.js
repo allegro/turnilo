61bb35fc378022fff068f3ed930a0bd0a78c9263
Adrian Mróź <adrianmroz@users.noreply.github.com>
2019-11-29 11:51:33 +0100

    Babel, webpack and new versions of imply libs (#520)
    * Compile code to ES6 with Babel instead of TypeScript
    
    * Use consistent quotes
    
    * Update imply packages
    
    * Transpile JavaScript files from node_modules with Babel
    
    * Upgrade Webpack
    
    * Transpile server code to ES5 and CommonJS
    
    * DEBUG
    
    * Make sure there is only one chronoshift module across the app
    
    * Add mode production flag and move sourcemap setting to the common config
    
    * Get rid of chunks, move D&D polyfill code to separate module
    
    * lint fixes

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -14,15 +14,16 @@
  * limitations under the License.
  */
 
-const common = require("./webpack.common");
-const merge = require("webpack-merge");
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge.smart(common, {
+  mode: "production",
   entry: {
     main: ["./src/client/main.tsx"]
   },
-  devtool: "source-map",
   module: {
     rules: [
       {
@@ -38,10 +39,13 @@ module.exports = merge.smart(common, {
       },
    ]
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
   ]
 });
