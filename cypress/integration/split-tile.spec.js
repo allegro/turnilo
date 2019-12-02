407f67a6b329437929aad9248fa9cbe815415cba
Adrian Mroz <adrian.mroz@allegro.pl>
2019-12-02 13:59:55 +0100

    Split Menus specs

--- cypress/integration/split-tile.spec.js
+++ cypress/integration/split-tile.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 context('Split Tile', () => {
-  describe("No splits in View Definition", () => {
+  describe('No splits in View Definition', () => {
     beforeEach(() => {
       cy.visit('http://localhost:9090');
     });
@@ -57,7 +57,7 @@ context('Split Tile', () => {
         cy.get('.search-box input').type('Channel');
         cy.get('.rows .dimension')
           .should('have.length', 1)
-          .should('contain', "Channel")
+          .should('contain', 'Channel')
           .click();
       });
       // Select Add Split action
@@ -69,7 +69,7 @@ context('Split Tile', () => {
     });
   });
 
-  describe("Channel split already in View Definition", () => {
+  describe('Channel split already in View Definition', () => {
     beforeEach(() => {
       cy.visit('http://localhost:9090/#wiki/4/N4IgbglgzgrghgGwgLzgFwgewHYgFwhpwBGCApiADSEQC2ZyOFBAomgMYD0AqgCoDCVEADMICNGQBOUfAG1QaAJ4AHZjXpDJZYfnUVqGegAUpWACYy88kGZiT0WXASMBGACJCoE5fgC0LgxU1BHQyLxAAXwBdaOooZSQ0S2slVV0vSQhsAHMhMzoybChHXXYACzhsbDIET0xJNHxQLR0CODMzMjMhVLUoUzC8iC12DBxdTqh2QvycoVVMzG6CSOokWghGvABWAAZYkH7Mwatm7SlC6d12zu7qYXradCbCIIntOBhxITBEGDVVq80gR6HBYFpIlFqMostUzG4CkVHMkQOVKtVatRsHB6PE4FdqNAAEqYYiYRpQkAwqpdADK9S2IBuXSE5GyM3w2C+CGoZQg2TKSAFWy5CAQESAA==');
     });
@@ -85,7 +85,7 @@ context('Split Tile', () => {
       cy.get('.split-tile .add-tile').click();
 
       cy.get('.add-tile-menu .tile-row:contains("Channel")')
-        .should("not.exist");
+        .should('not.exist');
     });
 
     it('should add split with plus button', () => {
@@ -154,7 +154,7 @@ context('Split Tile', () => {
     });
   });
 
-  describe("Three splits already in View Definition", () => {
+  describe('Three splits already in View Definition', () => {
     beforeEach(() => {
       // Force viewport that shows overflows for three tiles
       cy.viewport(1200, 800);
@@ -176,7 +176,191 @@ context('Split Tile', () => {
         .click();
 
       cy.get('.overflow-menu .split.dimension')
+        .should('contain', 'City Name');
+    });
+
+    it('should open split menu inside overflow tile', () => {
+      cy.get('.split-tile .items .dimension.overflow')
+        .click();
+
+      cy.get('.overflow-menu .split.dimension')
+        .click();
+
+      cy.get('.split-menu').should('exist');
+    });
+  });
+
+  describe('Remove action', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:9090/#wiki/4/N4IgbglgzgrghgGwgLzgFwgewHYgFwhpwBGCApiADSEQC2ZyOFBAomgMYD0AqgCoDCVEADMICNGQBOUfAG1QaAJ4AHZjXpDJZYfnUVqGegAUpWACYy88kGZiT0WXASMBGACJCoE5fgC0LgxU1BHQyLxAAXwBdaOooZSQ0S2slVV0vSQhsAHMhMzoybChHXXYACzhsbDIET0xJNHxQLR0CODMzMjMhVLUoUzC8iC12DBxdTqh2QvycoVVMzG6CSOokWghGvABWAAYIygUg9LRMuep8+iKSgmU4bP0QKHqt5u1dds7uwLSCfszBhdhmRRjcbGFpthZrlqAtzLpViB1pt8NsDkdfk9TlkYTYCtdxgR2JtFAA5OAaOIvJogFofDpdHrHP4DGRAkZjJzgqYzHHzUxLBEHJF0FE7WJPVlyN7CKSFab0r5CYT1WjoGm9CbaOAwcRCMCIGBqRGagj0OCwLSRKKwrLVMxufHFHDJEDlSrVWrUbAUsJ3BXUaAAJUwxEwjRtIGUdq6AGVqW0Gd8kWQHlD8NhdQhqGUINkykh81tMwgEBEgA');
+    });
+
+    it('should remove split after clicking "x" icon', () => {
+      cy.get('.split-tile .items').within(() => {
+        cy.get('.split.dimension:contains("Page") .remove').click();
+
+        cy.get('.split.dimension')
+          .should('have.length', 2);
+
+        cy.get('.split.dimension:first')
+          .should('contain', 'Channel');
+
+        cy.get('.split.dimension:nth-child(2)')
           .should('contain', 'City Name');
+      });
+    });
+  });
+
+  describe('Split menu', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:9090/#wiki/4/N4IgbglgzgrghgGwgLzgFwgewHYgFwhpwBGCApiADSEQC2ZyOFBAomgMYD0AqgCoDCVEADMICNGQBOUfAG1QaAJ4AHZjXpDJZYfnUVqGegAUpWACYy88kGZiT0WXASMBGACJCoE5fgC0LgxU1BHQyLxAAXwBdaOooZSQ0S2slVV0vSQhsAHMhMzoybChHXXYACzhsbDIET0xJNHxQLR0CODMzMjMhVLUoUzC8iC12DBxdTqh2QvycoVVMzG6CSOokWghGvABWAAYIygUg3UN9GwKikoJToWz7bBgQzKVdI14XAAk6hqaQFpOCj1jgR8vRLuNqPkRmMnCA4FMZllcgcQOtNvgHggEAcjmkCA9aMQpEMwcVxgR2JhaGC0AAZQrZNBlW73R5wZ6KfAuXZxepbZraXTtTrdQJ4kD9TKDSHDMijK42MLTbCzXLUBbmXSrVF0dE7WISgbJAXCKSFaZCjpdITCeq0dC/XoTbRwR6NahgRAwNTap0EejwuwUA3KLLVMxuC5kopyEDlSrVWrUbBwejxOAW6jQABKmGImEaUXVYa6AGU+ZaRUJyNkZhjHghqGUINkykhW1tMdigA==');
+    });
+
+
+    it('split menu has action buttons', () => {
+      cy.get('.split.dimension:contains("Channel")').click();
+
+      cy.get('.button-bar .primary').should('contain', 'OK');
+      cy.get('.button-bar .secondary').should('contain', 'Cancel');
+    });
+
+    it('split menu OK action should be disabled upon menu opening', () => {
+      cy.get('.split.dimension:contains("Channel")').click();
+
+      cy.get('.button-bar .primary').should('have.attr', 'disabled');
+    });
+
+    it('split menu OK action should be enabled after changing split options', () => {
+      cy.get('.split.dimension:contains("Channel")').click();
+
+      cy.get('.sort-direction .direction').click();
+
+      cy.get('.button-bar .primary').should('not.have.attr', 'disabled');
+    });
+
+    describe('String Split menu', () => {
+      it('should show split menu for string dimension', () => {
+        cy.get('.split.dimension:contains("Channel")').click();
+
+        cy.get('.split-menu').should('exist');
+      });
+
+      it('string split menu has sort controls', () => {
+        cy.get('.split.dimension:contains("Channel")').click();
+
+        cy.get('.split-menu .sort-direction').within(() => {
+          cy.get('.direction').should('have.class', 'descending');
+          cy.get('.dropdown-label').should('contain', 'Sort by');
+          cy.get('.dropdown .selected-item').should('contain', 'Added');
+        });
+      });
+
+      it('string split menu has limit controls', () => {
+        cy.get('.split.dimension:contains("Channel")').click();
+
+        cy.get('.split-menu > .dropdown.down').within(() => {
+          cy.get('.dropdown-label').should('contain', 'Limit');
+          cy.get('.selected-item').should('contain', '50');
+        });
+      });
+    });
+
+    describe('Time Split menu', () => {
+      it('should show split menu for time dimension', () => {
+        cy.get('.split.dimension:contains("Time")').click();
+
+        cy.get('.split-menu').should('exist');
+      });
+
+      it('time split menu has granularity controls', () => {
+        cy.get('.split.dimension:contains("Time")').click();
+
+        cy.get('.split-menu .button-group').within(() => {
+          cy.get('.button-group-title').should('contain', 'Granularity');
+          cy.get('.group-container .group-member').should('have.length', 6);
+          cy.get('.group-container .group-member.selected').should('exist');
+          cy.get('.group-container .group-member:last').should('contain', '…');
+        });
+      });
+
+      it('granularity "…" option show input box', () => {
+        cy.get('.split.dimension:contains("Time")').click();
+
+        cy.get('.split-menu .button-group .group-member:last').click();
+
+        cy.get('.split-menu .custom-input')
+          .should('have.class', 'invalid')
+          .should('have.value', '')
+          .should('have.attr', 'placeholder', 'e.g. PT2H or P3M')
+      });
+
+      it('time split menu has sort controls', () => {
+        cy.get('.split.dimension:contains("Time")').click();
+
+        cy.get('.split-menu .sort-direction').within(() => {
+          cy.get('.direction').should('have.class', 'ascending');
+          cy.get('.dropdown-label').should('contain', 'Sort by');
+          cy.get('.dropdown .selected-item').should('contain', 'Time');
+        });
+      });
+
+      it('time split menu has limit controls', () => {
+        cy.get('.split.dimension:contains("Time")').click();
+
+        cy.get('.split-menu > .dropdown.down').within(() => {
+          cy.get('.dropdown-label').should('contain', 'Limit');
+          cy.get('.selected-item').should('contain', 'None');
+        });
+      });
+    });
+
+    describe('Number Split menu', () => {
+      it('should show split menu for number dimension', () => {
+        cy.get('.split.dimension:contains("Comment")').click();
+
+        cy.get('.split-menu').should('exist');
+      });
+
+      it('number split menu has granularity controls', () => {
+        cy.get('.split.dimension:contains("Comment")').click();
+
+        cy.get('.split-menu .button-group').within(() => {
+          cy.get('.button-group-title').should('contain', 'Granularity');
+          cy.get('.group-container .group-member').should('have.length', 6);
+          cy.get('.group-container .group-member.selected').should('exist');
+          cy.get('.group-container .group-member:last').should('contain', '…');
+        });
+      });
+
+      it('granularity "…" option show input box', () => {
+        cy.get('.split.dimension:contains("Comment")').click();
+
+        cy.get('.split-menu .button-group .group-member:last').click();
+
+        cy.get('.split-menu .custom-input')
+          .should('have.class', 'invalid')
+          .should('have.value', '')
+          .should('have.attr', 'placeholder', 'Bucket size')
+      });
+
+      it('number split menu has sort controls', () => {
+        cy.get('.split.dimension:contains("Comment")').click();
+
+        cy.get('.split-menu .sort-direction').within(() => {
+          cy.get('.direction').should('have.class', 'descending');
+          cy.get('.dropdown-label').should('contain', 'Sort by');
+          cy.get('.dropdown .selected-item').should('contain', 'Added');
+        });
+      });
+
+      it('number split menu has limit controls', () => {
+        cy.get('.split.dimension:contains("Comment")').click();
+
+        cy.get('.split-menu > .dropdown.down').within(() => {
+          cy.get('.dropdown-label').should('contain', 'Limit');
+          cy.get('.selected-item').should('contain', 5);
+        });
+      });
     });
   });
 });
