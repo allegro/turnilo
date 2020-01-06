06fc5339aa711de03cf92c4797e3da604f227ad3
Richard Jones <itszootime@gmail.com>
2020-01-06 14:43:07 +0100

    remove superfluous plywood requests counter
    the request duration histogram also contains the count

--- src/server/routes/plywood/plywood.ts
+++ src/server/routes/plywood/plywood.ts
@@ -31,10 +31,7 @@ export function plywoodRouter(getSettings: SettingsGetter) {
     const stopRequestTimer = metrics.plywoodRequestDuration.startTimer();
 
     onFinished(res, function(err, res) {
-      const lables = { dataCube: req.body.dataCube, status: res.statusCode };
-
-      metrics.plywoodRequests.inc(lables);
-      stopRequestTimer(lables);
+      stopRequestTimer({ dataCube: req.body.dataCube, status: res.statusCode });
     });
 
     if (done) {
--- src/server/utils/prom-metrics/prom-metrics.ts
+++ src/server/utils/prom-metrics/prom-metrics.ts
@@ -1,11 +1,5 @@
 import { Counter, Histogram } from "prom-client";
 
-export const plywoodRequests = new Counter({
-  name: "turnilo_plywood_requests",
-  help: "Total number of requests recieved on plywood endpoint",
-  labelNames: ["dataCube", "status"]
-});
-
 export const plywoodRequestDuration = new Histogram({
   name: "turnilo_plywood_request_duration_seconds",
   help: "Request duration for plywood queries",
