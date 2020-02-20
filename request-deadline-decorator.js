6bac207b4db8e3533d73031908c54310813e5008
alexmorten <alexandrius.martinus@gmail.com>
2020-02-20 14:54:59 +0100

    use const instead of var

--- request-deadline-decorator.js
+++ request-deadline-decorator.js
@@ -3,7 +3,7 @@ exports.version = 1;
 exports.druidRequestDecoratorFactory = function (logger, params) {
   return function (decoratorRequest, context) {
     if (context && context.requestDeadline) {
-      var now = new Date().valueOf()
+      const now = new Date().valueOf()
       if (now > context.requestDeadline) {
         throw new Error('request deadline overstepped')
       }
