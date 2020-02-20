d457fb181bd25a86d729e38ae9929f87afdc8176
alexmorten <alexandrius.martinus@gmail.com>
2020-02-20 14:54:48 +0100

    remove unnecessary query in decorator

--- request-deadline-decorator.js
+++ request-deadline-decorator.js
@@ -2,17 +2,11 @@ exports.version = 1;
 
 exports.druidRequestDecoratorFactory = function (logger, params) {
   return function (decoratorRequest, context) {
-    var query = Object.assign({}, decoratorRequest.query)
-
     if (context && context.requestDeadline) {
       var now = new Date().valueOf()
       if (now > context.requestDeadline) {
         throw new Error('request deadline overstepped')
       }
-    }
-
-    return {
-      query: query,
     }
   };
 }
