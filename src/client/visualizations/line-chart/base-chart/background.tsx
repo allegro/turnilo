bfe55e4dcea7b8f0a8e0099aafee9b66f0e4cfba
Adrian Mroz <adrian.mroz@allegro.pl>
2020-04-22 12:30:43 +0200

    some roundings for crisper svg lines

--- src/client/visualizations/line-chart/base-chart/background.tsx
+++ src/client/visualizations/line-chart/base-chart/background.tsx
@@ -20,6 +20,7 @@ import { Stage } from "../../../../common/models/stage/stage";
 import { Unary } from "../../../../common/utils/functional/functional";
 import { GridLines } from "../../../components/grid-lines/grid-lines";
 import { VerticalAxis } from "../../../components/vertical-axis/vertical-axis";
+import { roundToHalfPx } from "../../../utils/dom/dom";
 import { ContinuousTicks } from "../utils/pick-x-axis-ticks";
 import { ContinuousScale } from "../utils/scale";
 import "./background.scss";
@@ -36,7 +37,7 @@ interface BackgroundProps {
 type Linear = scale.Linear<number, number>;
 
 function calculateTicks(scale: Linear) {
-  return scale.ticks(5).filter((n: number) => n !== 0);
+  return scale.ticks(5).filter(n => n !== 0);
 }
 
 const TICK_WIDTH = 5;
@@ -51,6 +52,7 @@ export const Background: React.SFC<BackgroundProps> = props => {
       ticks={calculateTicks(yScale)}
       stage={gridStage}
     />;
+    {/* omit last xTick if it's equal to last data point so we don't overplot with yAxis */}
     <GridLines
       orientation="vertical"
       scale={xScale}
@@ -67,10 +69,10 @@ export const Background: React.SFC<BackgroundProps> = props => {
     <line
       className="vis-bottom"
       transform={gridStage.getTransform()}
-      x1="0"
+      x1={0}
       x2={gridStage.width + TICK_WIDTH}
-      y1={gridStage.height - 0.5}
-      y2={gridStage.height - 0.5}
+      y1={roundToHalfPx(gridStage.height)}
+      y2={roundToHalfPx(gridStage.height)}
     />
   </React.Fragment>;
 };
