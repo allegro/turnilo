17c45a1c6a752470369df40b6ffdb2ccb06d88c6
Adrian Mroz <adrian.mroz@allegro.pl>
2020-04-23 16:39:09 +0200

    Add SeriesLegend to ChartsPerSplit component.

--- src/client/visualizations/line-chart/charts/charts-per-split/charts-per-split.tsx
+++ src/client/visualizations/line-chart/charts/charts-per-split/charts-per-split.tsx
@@ -14,15 +14,17 @@
  * limitations under the License.
  */
 
-import { Dataset, Datum, PlywoodValue } from "plywood";
+import { Dataset, Datum } from "plywood";
 import * as React from "react";
 import { Essence } from "../../../../../common/models/essence/essence";
 import { Stage } from "../../../../../common/models/stage/stage";
-import { compose } from "../../../../../common/utils/functional/functional";
+import { compose, Unary } from "../../../../../common/utils/functional/functional";
+import { LegendSpot } from "../../../../components/pinboard-panel/pinboard-panel";
 import { SPLIT } from "../../../../config/constants";
+import { SeriesLegend } from "../../legend/series-legend";
 import { ContinuousTicks } from "../../utils/pick-x-axis-ticks";
 import { ContinuousScale } from "../../utils/scale";
-import { getNominalSplit, hasNominalSplit } from "../../utils/splits";
+import { hasNominalSplit } from "../../utils/splits";
 import { calculateChartStage } from "../calculate-chart-stage";
 import { SplitChart } from "./split-chart";
 
@@ -34,39 +36,35 @@ interface ChartsPerSplit {
   stage: Stage;
 }
 
+function getChartsSelectors(essence: Essence, dataset: Dataset): Array<Unary<Dataset, Datum>> {
+  const get1stLevel = (dataset: Dataset) => dataset.data[0];
+  if (!hasNominalSplit(essence)) {
+    return [get1stLevel];
+  }
+
+  const splitDatums = (get1stLevel(dataset)[SPLIT] as Dataset).data;
+  return splitDatums.map((datum, index) => {
+    const get2ndLevel = (datum: Datum) => (datum[SPLIT] as Dataset).data[index];
+    return compose<Dataset, Datum, Datum>(get1stLevel, get2ndLevel);
+  });
+}
+
 export const ChartsPerSplit: React.SFC<ChartsPerSplit> = props => {
   const { xScale, xTicks, essence, dataset, stage } = props;
 
-  const get1stLevel = (dataset: Dataset) => dataset.data[0];
-
-  if (!hasNominalSplit(essence)) {
-    const chartStage = calculateChartStage(stage, 1);
-    return <SplitChart
+  const hasMultipleSeries = essence.series.count() > 1;
+  const selectors = getChartsSelectors(essence, dataset);
+  const chartStage = calculateChartStage(stage, selectors.length);
+  return <React.Fragment>
+    {hasMultipleSeries && <LegendSpot>
+      <SeriesLegend essence={essence} />
+    </LegendSpot>}
+    {selectors.map(selector => <SplitChart
       essence={essence}
       dataset={dataset}
-      selectDatum={get1stLevel}
+      selectDatum={selector}
       xScale={xScale}
       xTicks={xTicks}
-      chartStage={chartStage} />;
-  }
-
-  const nominalSplit = getNominalSplit(essence);
-  const splitDatums = (get1stLevel(dataset)[SPLIT] as Dataset).data;
-  const chartStage = calculateChartStage(stage, splitDatums.length);
-
-  return <React.Fragment>
-    {splitDatums.map((datum, index) => {
-      const get2ndLevel = (datum: Datum) => (datum[SPLIT] as Dataset).data[index];
-      const getSubDataset = compose<Dataset, Datum, Datum>(get1stLevel, get2ndLevel);
-      const key = datum[nominalSplit.reference] as PlywoodValue;
-      return <SplitChart
-        key={String(key)}
-        essence={essence}
-        selectDatum={getSubDataset}
-        chartStage={chartStage}
-        xScale={xScale}
-        xTicks={xTicks}
-        dataset={dataset} />;
-    })}
+      chartStage={chartStage} />)}
   </React.Fragment>;
 };
