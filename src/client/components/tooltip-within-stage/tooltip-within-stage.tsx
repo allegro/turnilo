66fa00f5b70b9947480fe5f0511f0265c58bb25b
Adrian Mroz <adrian.mroz@allegro.pl>
2020-05-21 17:08:18 +0200

    Component for Tooltip that's guaranteed to render inside provided Stage

---
+++ src/client/components/tooltip-within-stage/tooltip-within-stage.tsx
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2017-2018 Allegro.pl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from "react";
+import { Stage } from "../../../common/models/stage/stage";
+import "./tooltip-within-stage.scss";
+
+interface TooltipWithinStageProps {
+  stage: Stage;
+  top: number;
+  left: number;
+  margin?: number;
+}
+
+interface TooltipWithinStageState {
+  rect?: ClientRect | DOMRect;
+}
+
+type Position = Pick<React.CSSProperties, "left" | "top">;
+
+export class TooltipWithinStage extends React.Component<TooltipWithinStageProps, TooltipWithinStageState> {
+
+  private self = React.createRef<HTMLDivElement>();
+
+  state: TooltipWithinStageState = {};
+
+  componentDidMount() {
+    this.setState({
+      rect: this.self.current.getBoundingClientRect()
+    });
+  }
+
+  calculatePosition(): Position {
+    const { top: initialTop, left: initialLeft, stage, margin = 10 } = this.props;
+    const { rect } = this.state;
+    if (!rect) {
+      const top = initialTop + margin;
+      const left = initialLeft + margin;
+      return { top, left };
+    }
+
+    const stageBottom = stage.y + stage.height;
+    const stageRight = stage.x + stage.width;
+
+    const top = rect.bottom > stageBottom
+      ? initialTop - margin - rect.height
+      : rect.top < stage.y
+      ? initialTop + rect.height
+      : initialTop + margin;
+
+    const left = rect.right > stageRight
+      ? initialLeft - margin - rect.width
+      : rect.left < stage.x
+      ? initialLeft + rect.width
+      : initialLeft + margin;
+
+    return { top, left };
+  }
+
+  render() {
+    const { children } = this.props;
+    return <div
+      className="tooltip-within-stage"
+      style={this.calculatePosition()}
+      ref={this.self}>
+      {children}
+    </div>;
+  }
+}
