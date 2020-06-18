0b7c101b8af7049112523ddd20f199a8a76c2e36
Adrian Mroz <adrian.mroz@allegro.pl>
2020-06-18 15:27:45 +0200

    Basic e2e for bar-chart

---
+++ cypress/integration/bar-chart.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+context("Bar Chart", () => {
+
+  const barChart = () => cy.get(".bar-chart");
+  const xAxis = () => cy.get(".bar-chart-x-axis");
+  const ticks = () => xAxis().find("g");
+
+  const urls = {
+    timeSplit: "http://localhost:9090/#wiki/4/N4IgbglgzgrghgGwgLzgFwgewHYgFwgBGcATgLQDGAFqWiADTjTxKoY4DKApmhtgOZR82GAgSMMAWy7IcXfCACiaCgHoAqgBUAwgxAAzCAjRcSQvAG1QaAJ4AHeQSnzGJLvoXO9zgAqmsACbmViABMCToWLgEPgCMACJ6UCZ2+GSxEvaOIAjoXMkgAL4AuiWMUHZIaMHWWZ4Q0noBDVzYUFH1jYz8ESK5JBC2Cj6asQASSZgkdHigbh5OLd51BM3SbR2MzW4U7NEgcFAUrc0CRYxIkoPCoghlIFD++fgh86atxwpwAQFcAXr6KaSdD4WoOBS/fRwUR0RhgRAwbLnEC2cEEaSHcLye52CDYbB/eItDY4YIgahwfFcBB6bBwaQVOCfRjQABKmEImDoxUYuKpAQ4UxmBx+fyKQA",
+  };
+
+  describe("Time split", () => {
+    beforeEach(() => {
+      cy.visit(urls.timeSplit);
+    });
+
+    it("should load bar-chart", () => {
+      barChart().should("exist");
+    });
+
+    it("should load five ticks", () => {
+      ticks().should("have.length", 5);
+    });
+  });
+});
