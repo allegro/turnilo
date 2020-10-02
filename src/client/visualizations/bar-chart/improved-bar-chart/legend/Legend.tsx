c297e45c83f27fc9e957046235afa32276656835
Adrian Mroz <adrian.mroz@allegro.pl>
2020-10-02 15:35:22 +0200

    Use proper import convention

--- src/client/visualizations/bar-chart/improved-bar-chart/legend/Legend.tsx
+++ src/client/visualizations/bar-chart/improved-bar-chart/legend/Legend.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React from "react";
+import * as React from "react";
 import { StackedMode } from "../utils/chart-mode";
 import "./legend.scss";
 
