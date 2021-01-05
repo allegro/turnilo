32132c18a2297b5a5be8caa049ecfa9f94251b46
Adrian Mroz <adrian.mroz@allegro.pl>
2021-01-05 16:55:17 +0100

    Launch old tests

--- src/common/models/timekeeper/timekeeper.mocha.ts
+++ src/common/models/timekeeper/timekeeper.mocha.ts
@@ -16,37 +16,38 @@
  */
 
 import { expect } from "chai";
+import { testImmutableClass } from "immutable-class-tester";
 
 import { Timekeeper } from "./timekeeper";
 
 describe("Timekeeper", () => {
   it("is an immutable class", () => {
-    /* TODO: check the correctness of the test */
-    /*    testImmutableClass(Timekeeper, [
-          {
-            timeTags: []
-          },
-          {
-            timeTags: [
-              { name: 'lol', time: new Date('2016-01-01T01:02:03Z'), updated: new Date('2016-01-01T01:02:03Z') }
-            ]
-          },
-          {
-            timeTags: [
-              { name: 'lol', time: new Date('2016-01-01T01:02:03Z'), updated: new Date('2016-01-01T01:02:03Z') }
-            ],
-            nowOverride: new Date('2016-01-01T01:02:03Z')
-          }
-        ]);*/
+    testImmutableClass(Timekeeper, [
+      {
+        timeTags: {},
+        nowOverride: null
+      },
+      {
+        timeTags: {
+          lol: { name: "lol", time: new Date("2016-01-01T01:02:03Z"), updated: new Date("2016-01-01T01:02:03Z") }
+        },
+        nowOverride: null
+      },
+      {
+        timeTags: {
+          lol: { name: "lol", time: new Date("2016-01-01T01:02:03Z"), updated: new Date("2016-01-01T01:02:03Z") }
+        },
+        nowOverride: new Date("2016-01-01T01:02:03Z")
+      }
+    ]);
   });
 
   it("works with now", () => {
-    var timekeeper = Timekeeper.fromJS({
-      timeTags: [],
+    const timekeeper = Timekeeper.fromJS({
+      timeTags: {},
       nowOverride: new Date("2016-01-01T01:02:03Z")
     });
 
     expect(timekeeper.now()).to.deep.equal(new Date("2016-01-01T01:02:03Z"));
   });
-
 });
