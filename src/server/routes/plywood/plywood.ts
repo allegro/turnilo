05529cb53caecb3cf0a93f328a3fdeb92fdfea95
Adrian Mroz <adrian.mroz@allegro.pl>
2021-01-27 14:37:48 +0100

    do not use cluster timeout on server plywood route

--- src/server/routes/plywood/plywood.ts
+++ src/server/routes/plywood/plywood.ts
@@ -86,10 +86,11 @@ export function plywoodRouter(settingsManager: Pick<SettingsManager, "anchorPath
       return null;
     }
 
-    // "native" clusters are not defined, maybe they should be defined as some stub object
-    if (myDataCube.cluster) {
-      req.setTimeout(myDataCube.cluster.getTimeout(), null);
-    }
+    /* TODO:
+    - get server timeout from ServerSettings
+    - apply timeout on the whole request-response
+    - if timeout is 0 - no timeout should be set
+     */
     const maxQueries = myDataCube.getMaxQueries();
     const decorator = loadQueryDecorator(myDataCube, settingsManager.anchorPath, LOGGER);
     const expression = decorator(parsedExpression, req);
