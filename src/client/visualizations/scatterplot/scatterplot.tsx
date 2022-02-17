bc3e172afc4ac27a9e0a2da2394ab1d5e95d32ed
Katarzyna Zadurska <katarzyna.zadurska@allegro.pl>
2022-02-17 16:33:40 +0100

    Adjust label position

--- src/client/visualizations/scatterplot/scatterplot.tsx
+++ src/client/visualizations/scatterplot/scatterplot.tsx
@@ -56,9 +56,11 @@ export class Scatterplot extends React.Component<ChartProps, ScatterplotState> {
 
     const { xTicks, yTicks, xScale, yScale, xSeries, ySeries, plottingStage, scatterplotData } = getPlottingData(data, essence, stage);
 
+    const rightXAxisLabelPosition = stage.width - (plottingStage.width + plottingStage.x);
+    const bottomXAxisLabelPosition  = stage.height - (plottingStage.height + plottingStage.y - 55); // magic number here
     return <div className="scatterplot-container" style={stage.getWidthHeight()}>
       <span className="axis-title" style={{ top: 10, left: 10 }}>{ySeries.title()}</span>
-      <span className="axis-title" style={{ bottom: 145, right: 10 }}>{xSeries.title()}</span>
+      <span className="axis-title" style={{ bottom: bottomXAxisLabelPosition, right: rightXAxisLabelPosition }}>{xSeries.title()}</span>
       <Tooltip datum={this.state.hoveredPoint} stage={plottingStage} ySeries={ySeries} xSeries={xSeries} yScale={yScale} xScale={xScale} splitKey={splitKey}/>
       <svg viewBox={stage.getViewBox()}>
         <GridLines orientation={"vertical"} stage={plottingStage} ticks={xTicks} scale={xScale} />
