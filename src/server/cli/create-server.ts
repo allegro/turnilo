37b537259f16206c263c036982af2d984b5a5b77
Adrian Mroz <adrian.mroz@allegro.pl>
2022-11-03 17:55:48 +0100

    create server as a function with explicit dependencies

--- src/server/cli/create-server.ts
+++ src/server/cli/create-server.ts
@@ -1,6 +1,5 @@
 /*
- * Copyright 2015-2016 Imply Data, Inc.
- * Copyright 2017-2019 Allegro.pl
+ * Copyright 2017-2022 Allegro.pl
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,15 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import * as http from "http";
-
+import { Express } from "express";
+import http from "http";
 import { AddressInfo } from "net";
-import app from "./app";
-import { SERVER_SETTINGS, START_SERVER } from "./config";
+import { ServerSettings } from "../models/server-settings/server-settings";
 
-if (START_SERVER) {
-  var server = http.createServer(app);
+export default function createServer(serverSettings: ServerSettings, app: Express) {
+
+  const server = http.createServer(app);
 
   server.on("error", (error: any) => {
     if (error.syscall !== "listen") {
@@ -32,12 +30,14 @@ if (START_SERVER) {
     // handle specific listen errors with friendly messages
     switch (error.code) {
       case "EACCES":
-        console.error(`Port ${SERVER_SETTINGS.port} requires elevated privileges`);
+        // TODO: this should do something that commander understands
+        console.error(`Port ${serverSettings.port} requires elevated privileges`);
         process.exit(1);
         break;
 
       case "EADDRINUSE":
-        console.error(`Port ${SERVER_SETTINGS.port} is already in use`);
+        // TODO: this should do something that commander understands
+        console.error(`Port ${serverSettings.port} is already in use`);
         process.exit(1);
         break;
 
@@ -51,6 +51,6 @@ if (START_SERVER) {
     console.log(`Turnilo is listening on address ${address.address} port ${address.port}`);
   });
 
-  app.set("port", SERVER_SETTINGS.port);
-  server.listen(SERVER_SETTINGS.port, SERVER_SETTINGS.serverHost);
+  app.set("port", serverSettings.port);
+  server.listen(serverSettings.port, serverSettings.serverHost);
 }
