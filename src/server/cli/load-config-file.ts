daee927f3fe23c3e3d814b99f0e76b214580881e
Adrian Mroz <adrian.mroz@allegro.pl>
2022-11-03 17:56:08 +0100

    util for loading file, soon will talk to commander

---
+++ src/server/cli/load-config-file.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2017-2022 Allegro.pl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { loadFileSync } from "../utils/file/file";
+
+export function loadConfigFile(configPath: string): object {
+  try {
+    return loadFileSync(configPath, "yaml");
+  } catch (e) {
+    throw new Error("SOMETHING"); // something that commander can handle
+  }
+}
