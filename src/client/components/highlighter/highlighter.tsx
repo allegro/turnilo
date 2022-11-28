78ffdebb4dd32362f44ae15fe3fc1514f11b59b4
Piotr Wysocki <86244209+piwysocki@users.noreply.github.com>
2022-11-28 11:41:14 +0100

    tslint: enforce self closing of empty jsx elements (#972)

--- src/client/components/highlighter/highlighter.tsx
+++ src/client/components/highlighter/highlighter.tsx
@@ -44,8 +44,8 @@ export function Highlighter(props: HighlighterProps) {
   };
 
   return <div className="highlighter">
-    <div className="whiteout left" style={whiteoutLeftStyle}></div>
-    <div className="frame" style={frameStyle}></div>
-    <div className="whiteout right" style={whiteoutRightStyle}></div>
+    <div className="whiteout left" style={whiteoutLeftStyle} />
+    <div className="frame" style={frameStyle} />
+    <div className="whiteout right" style={whiteoutRightStyle} />
   </div>;
 }
