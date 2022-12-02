5b3f11579cd91f313907e74bd87ad4c99e65e0b5
Adrian Mroz <adrian.mroz@allegro.pl>
2022-12-02 15:38:32 +0100

    simplify timeseries split tile

--- src/client/components/timeseries-visualization-controls/visualization-controls.tsx
+++ src/client/components/timeseries-visualization-controls/visualization-controls.tsx
@@ -15,68 +15,13 @@
  */
 
 import React from "react";
-import { classNames } from "../../utils/dom/dom";
-import {
-  VisualizationControls,
-  VisualizationControlsBaseProps
-} from "../../views/cube-view/center-panel/center-panel";
-import { SplitTileBaseProps } from "../split-tile/split-tile";
+import { VisualizationControls, VisualizationControlsBaseProps } from "../../views/cube-view/center-panel/center-panel";
+import { SplitTile, SplitTileBaseProps } from "../split-tile/split-tile";
 import { SplitTilesRow, SplitTilesRowBaseProps } from "../split-tile/split-tiles-row";
-import { SvgIcon } from "../svg-icon/svg-icon";
-import { WithRef } from "../with-ref/with-ref";
 import { TimeSeriesSplitMenu } from "./split-menu";
 
-const TimeSeriesSplitTile: React.FunctionComponent<SplitTileBaseProps> = props => {
-  const {
-    essence,
-    open,
-    split,
-    dimension,
-    style,
-    removeSplit,
-    updateSplit,
-    openMenu,
-    closeMenu,
-    dragStart,
-    containerStage
-  } = props;
-
-  const title = split.getTitle(dimension);
-
-  const remove = (e: React.MouseEvent<HTMLElement>) => {
-    e.stopPropagation();
-    removeSplit(split);
-  };
-
-  return <WithRef>
-    {({ ref: openOn, setRef }) => <React.Fragment>
-      <div
-        className={classNames("tile dimension")}
-        key={split.toKey()}
-        ref={setRef}
-        draggable={true}
-        onClick={() => openMenu(split)}
-        onDragStart={e => dragStart(dimension.title, split, e)}
-        style={style}
-        title={title}
-      >
-        <div className="reading">{title}</div>
-        <div className="remove"
-             onClick={remove}>
-          <SvgIcon svg={require("../../icons/x.svg")}/>
-        </div>
-      </div>
-      {open && openOn && <TimeSeriesSplitMenu
-        saveSplit={updateSplit}
-        essence={essence}
-        openOn={openOn}
-        containerStage={containerStage}
-        onClose={closeMenu}
-        dimension={dimension}
-        split={split}/>}
-    </React.Fragment>}
-  </WithRef>;
-};
+const TimeSeriesSplitTile: React.FunctionComponent<SplitTileBaseProps> = props =>
+  <SplitTile splitMenuComponent={TimeSeriesSplitMenu} {...props} />;
 
 const TimeSeriesSplitTilesRow: React.FunctionComponent<SplitTilesRowBaseProps> = props =>
   <SplitTilesRow {...props} splitTileComponent={TimeSeriesSplitTile}/>;
