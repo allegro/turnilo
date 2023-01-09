d344d508cffd003d0e73523166f047d76b4482a2
Adrian Mroz <adrian.mroz@allegro.pl>
2023-01-09 21:35:27 +0100

    unit test for error handling

--- src/client/utils/ajax/ajax.mocha.ts
+++ src/client/utils/ajax/ajax.mocha.ts
@@ -20,36 +20,68 @@ import * as sinon from "sinon";
 import { Ajax } from "./ajax";
 
 describe("Ajax", () => {
-  let get: sinon.SinonStub;
-
-  beforeEach(() => {
-    get = sinon.stub(axios, "get");
-    get.withArgs("sources/dataCubes?page=0")
-      .resolves({ data: { dataCubes: ["first-cube", "second-cube"], next: 1 } });
-    get.withArgs("sources/dataCubes?page=1")
-      .resolves({ data: { dataCubes: ["third-cube", "fourth-cube"], next: 2 } });
-    get.withArgs("sources/dataCubes?page=2")
-      .resolves({ data: { dataCubes: ["fifth-cube", "sixth-cube"], next: 3 } });
-    get.withArgs("sources/dataCubes?page=3")
-      .resolves({ data: { dataCubes: ["last-cube"] } });
-  });
-
-  afterEach(() => {
-    get.restore();
-  });
 
   describe("fetchDataCubes", () => {
-    it("should work", async () => {
-      const dataCubes = await Ajax.fetchDataCubes({});
-      expect(dataCubes).to.be.deep.equal([
-        "first-cube",
-        "second-cube",
-        "third-cube",
-        "fourth-cube",
-        "fifth-cube",
-        "sixth-cube",
-        "last-cube"
-      ]);
+    let get: sinon.SinonStub;
+
+    describe("happy path", () => {
+      before(() => {
+        get = sinon.stub(axios, "get");
+        get.withArgs("sources/dataCubes?page=0")
+          .resolves({ data: { dataCubes: ["first-cube", "second-cube"], next: 1 } });
+        get.withArgs("sources/dataCubes?page=1")
+          .resolves({ data: { dataCubes: ["third-cube", "fourth-cube"], next: 2 } });
+        get.withArgs("sources/dataCubes?page=2")
+          .resolves({ data: { dataCubes: ["fifth-cube", "sixth-cube"], next: 3 } });
+        get.withArgs("sources/dataCubes?page=3")
+          .resolves({ data: { dataCubes: ["last-cube"] } });
+      });
+
+      it("should fetch all data cubes across pages", async () => {
+        const dataCubes = await Ajax.fetchDataCubes({});
+        expect(dataCubes).to.be.deep.equal([
+          "first-cube",
+          "second-cube",
+          "third-cube",
+          "fourth-cube",
+          "fifth-cube",
+          "sixth-cube",
+          "last-cube"
+        ]);
+      });
+
+      after(() => {
+        get.restore();
+      });
+    });
+
+    describe("error handling", () => {
+      before(() => {
+        get = sinon.stub(axios, "get");
+        get.withArgs("sources/dataCubes?page=0")
+          .resolves({ data: { dataCubes: ["first-cube", "second-cube"], next: 1 } });
+        get.withArgs("sources/dataCubes?page=1")
+          .rejects(new Error("Couldn't fetch"));
+      });
+
+      /*
+       NOTE:
+       Couldn't write this test with any fancy helpers.
+       If you try to refactor it, please check if your solution isn't a false positive!
+      */
+      it("should rethrow network error", async () => {
+        let error;
+        try {
+          await Ajax.fetchDataCubes({});
+        } catch (e) {
+          error = e;
+        }
+        expect(error).to.have.property("message", "Couldn't fetch");
+      });
+
+      after(() => {
+        get.restore();
+      });
     });
   });
 });
