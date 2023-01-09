ce0b95b9b7c34a48467c4966dc16baf0be06ad6c
Piotr Wysocki <86244209+piwysocki@users.noreply.github.com>
2023-01-09 16:39:25 +0100

    jsdom rollback

--- src/server/utils/file/file.ts
+++ src/server/utils/file/file.ts
@@ -18,12 +18,12 @@
 import * as fs from "fs";
 import * as yaml from "js-yaml";
 
-export function loadFileSync(filepath: string, postProcess: "json" | "yaml"): object {
+export function loadFileSync(filepath: string, postProcess: "json" | "yaml"): object | string | number | null | undefined {
   const fileData = fs.readFileSync(filepath, "utf-8");
   switch (postProcess) {
     case "json":
       return JSON.parse(fileData);
     case "yaml":
-      return yaml.safeLoad(fileData);
+      return yaml.load(fileData);
   }
 }
